ee562d65f685c02849e0de9da786a86dac59f14c
semtio <semik20092@gmail.com>
2025-09-05 21:06:55 +0200

    robots priority my plugin 999

--- index-or-not.php
+++ index-or-not.php
@@ -7,28 +7,33 @@ Author: 7on
 */
 
 // запрет прямого доступа
-if ( ! defined( 'ABSPATH' ) ) { exit; }
+if (! defined('ABSPATH')) {
+    exit;
+}
 
-function custom_control_robots_meta( $robots ) {
+function custom_control_robots_meta($robots)
+{
     // Сначала убираем возможные конфликты от темы/плагинов/ядра
-    unset( $robots['index'], $robots['noindex'], $robots['follow'], $robots['nofollow'] );
+    unset($robots['index'], $robots['noindex'], $robots['follow'], $robots['nofollow']);
 
-    $temp_page = array( 'home-page' ); // замените на нужные ярлыки/ID
+    $temp_page = array('home-page'); // замените на нужные ярлыки/ID
 
-    if ( is_page( $temp_page ) ) {
+    if (is_page($temp_page)) {
         // Разрешённая страница
         $robots['index']  = true;
         $robots['follow'] = true; // можно и не указывать — по умолчанию "follow"
     } else {
         // Все остальные
         $robots['noindex'] = true;
-		$robots['nofollow'] = true;
+        $robots['nofollow'] = true;
         // НЕ ставим nofollow, чтобы не резать краулинг ссылок
     }
 
     return $robots;
 }
 
-// высокий приоритет, чтобы перебить Yoast/RankMath/ядро
-add_filter( 'wp_robots', 'custom_control_robots_meta', 999 );
-
+$saintsmedia_index_or_not = $is_enabled = get_theme_mod('saintsmedia_index_or_not', false);
+if ($saintsmedia_index_or_not) {
+    // высокий приоритет, чтобы перебить Yoast/RankMath/ядро
+    add_filter('wp_robots', 'custom_control_robots_meta', 999);
+}
